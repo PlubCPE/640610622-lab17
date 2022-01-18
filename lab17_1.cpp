ecd99caf0dccbfdd1554d61d71c3e34c2d42325a
PlubCPE <chakkapop_s@cmu.ac.th>
2022-01-18 18:38:15 +0700

    lab17_1_complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,46 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *x, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << x[i];
+        if((i+1)%M == 0)  cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *x,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        x[i] = (rand()%101)/100.00;
+    }
+}
+
+void findRowSum(const double *x,double *sum1,int N,int M)
+{
+    for(int i = 0 ;i < N; i ++)
+        {
+            double sum = 0;
+            for(int j = 0 ; j < M ; j++)
+                {
+                    sum += x[i*M+j];
+                    sum1[i] = sum;
+                }
+        }
+                    
+
+}
+
+void findColSum(const double *x,double *sum2,int N,int M)
+{
+     for(int i = 0 ;i < M; i ++)
+        {
+            double sum = 0;
+            for(int j = 0 ; j < N ; j++)
+                {
+                    sum += x[j*M+i];
+                    sum2[i] = sum;
+                }
+        }
+}
